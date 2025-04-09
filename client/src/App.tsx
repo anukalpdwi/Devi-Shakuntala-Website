199de8f85c8ea8c68bae14dbdc291dd621c5dfe7
Anukalp Dwivedi <anukalpdwi1995@gmail.com>
2025-04-09 12:43:18 +0530

    alias changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Switch, Route, useLocation } from "wouter";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "./components/ui/toaster";
 import { useEffect } from "react";
 
 import Home from "@/pages/Home";
